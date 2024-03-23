2cf96414315c1c8f0ce79a4234b576c92fdf6ae3
Yash <seehra.a@yahoo.com>
2024-03-24 00:42:26 +0100

    added data files

--- db.js
+++ db.js
@@ -7,13 +7,14 @@ const sequelize = new Sequelize(
     process.env.DB_USER, // Database username
     process.env.DB_PASSWORD, // Database password
     {
-        host: String(process.env.DB_HOST), // Database host
+        host: process.env.DB_HOST, // Database host
         dialect: 'postgres'
     }
 )
 
 const connectDb = async () => {
     try {
+        console.log(process.env.DB_HOST)
         await sequelize.authenticate()
         console.log('Connection has been established successfully.')
     } catch (error) {
