a9672f91bc1686d3651112a3c79a14ae90c3dae4
Yash <seehra.a@yahoo.com>
2024-03-24 18:10:28 +0100

    making CRUD funcs

--- db.js
+++ db.js
@@ -12,13 +12,4 @@ const sequelize = new Sequelize(
     }
 )
 
-const connectDb = async () => {
-    try {
-        await sequelize.authenticate()
-        console.log('Connection has been established successfully.')
-    } catch (error) {
-        console.error('Unable to connect to the database:', error)
-    }
-}
-
-module.exports = { sequelize, connectDb }
+module.exports = sequelize
